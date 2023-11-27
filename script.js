e887dbaa10c916ede2b1eb790f070221c51b1ad8
Pradeep Bayereddy <vpradeepbayereddy@gmail.com>
2023-11-27 13:34:49 +0530

    Files to Github

---
+++ script.js
@@ -0,0 +1,11 @@
+function beforesubmit(){
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log("inputdate.value ", inputdate.value); //string --> date (en_IN) --> UserInfo.getLocale() in APEX
+
+
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+
+
+}
