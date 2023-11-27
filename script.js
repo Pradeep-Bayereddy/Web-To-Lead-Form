b7b172498bdeb572c44e74fdef58c425566a2be2
Pradeep Bayereddy <vpradeepbayereddy@gmail.com>
2023-11-27 21:05:25 +0530

    captcha validations

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function beforesubmit(){
-    let inputdate = document.querySelector(".inputdate");
-    let outputdate = document.querySelector(".outputdate");
-    console.log("inputdate.value ", inputdate.value); //string --> date (en_IN) --> UserInfo.getLocale() in APEX
+let captchaChecked = false;
 
+function beforesubmit(event){
 
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formattedDate;
+    if(captchaChecked){
+
+        let inputdate = document.querySelector(".inputdate");
+        let outputdate = document.querySelector(".outputdate");
+        console.log("inputdate.value ", inputdate.value); //string --> date (en_IN) --> UserInfo.getLocale() in APEX
+    
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formattedDate;
+
+    } else {
+        alert("Please check the ReCAPTCHA Box");
+        event.preventDefault();
+
+    }
 
 }
 
@@ -20,3 +30,7 @@ function timestamp() {
         } 
     } 
     setInterval(timestamp, 500); 
+
+    function captchaSuccess(){
+        captchaChecked = true;
+    }
